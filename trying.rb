6dc1f356d11c7f9d359d5e9bbbad2ab303be6e0e
Ben <BenRuns@users.noreply.github.com>
2014-05-12 22:31:29 -0700

    Delete trying.rb

--- trying.rb
+++
@@ -1,8 +0,0 @@
-#vowels = ("a".."z")
-# a = ("a".."z").each {|letter| vowels.include?(letter) }
-
-vowels = %w(a e i o u)
-consonant =("a".."z").find_all { |letter| !vowels.include? letter}
- 
-
-  
